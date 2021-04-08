027f9e3c56128f110a41949395eda6677e6e902c
docfate111 <tdwilliamsiv@gmail.com>
2021-04-07 18:22:58 -0700

    creating game and player classes

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # dice-10000
 TBD CS 181Y project (TODO: change this later)
+
+https://en.wikipedia.org/wiki/Dice_10000
+
+There are many variations but the rules I'm going with for now are:
+
+1. 3 ones is 1000, for 3 of a kind of 2-6 inclusive multiply by 100
+2. otherwise 1 is 100 and 5 is 50
+3. on the 1st turn you must roll until you reach 500 "to get on the board"
+4. if you use up all the dice you can roll again and those points are 
+adding to a running total
+
+# Add later
+1. If four, five, or six of a kind are rolled, each additional dice is worth as much again as the three of a kind score
+    This makes the highest possible score in a single roll 4000 for six ones (1000 for three ones, after that player gains 1000 points for each additional one in that series of rolling.) The ONE is the only dice you ever count in the thousands.
+2. A straight from 1 to 6 is worth 1500 points. If a player fails to roll a straight they may make one attempt to complete the straight. If the desired number(s) does not turn up on the next roll that round is a "crap out" even if there are scoring dice on the table i.e. 1's or 5's.
+3. Three pairs are worth 1500 points. For instance 2+2, 4+4, 5+5. This rule does not count if you roll a quadruple and a pair e.g. 2+2, 2+2, 6+6 unless stated otherwise (some places have their own house rules).
+4. If a player fails to roll a three pairs, they may make one attempt to complete the three of a kind. If the desired number(s) does not turn up on the next roll, that round is a "crap out", even if there are scoring dice on the table; i.e. 1's or 5's.
---
+++ test.py
@@ -0,0 +1,105 @@
+import random
+import os
+from collections import Counter
+
+def roll_n_die(n: int) -> list:
+    '''
+    simulate rolling the dice 3 times
+    '''
+    dice = []
+    for i in range(n):
+        dice.append(random.randint(1, 6))
+    dice.sort()
+    return dice
+
+def score_die(dice: list) -> tuple:
+    '''
+    return the score of the dice and dice remaining
+    '''
+    pts = 0
+    # check for triples
+    c = Counter(dice)
+    triples = []
+    seen = set()
+    for i, j in c.items():
+        if(j>=3):
+            triples.append(i)
+            if(i not in seen):
+                if(i==1):
+                    pts += 1000
+                else:
+                    pts += i*100
+            seen.add(i)
+    for elem in triples:
+        for i in range(3):
+            dice.remove(elem)
+    rest = []
+    while(1 in dice or 5 in dice):
+        if(dice[-1]==1):
+            pts += 100
+            dice.pop()
+        elif(dice[-1]==5):
+            pts += 50
+            dice.pop()
+        else:
+            rest.append(dice.pop())
+    print(dice)
+    return (pts, len(rest)+len(dice))     
+
+class Game:
+    def __init__(self):
+        random.seed(os.urandom(16))
+        self.num_of_players = 2
+        self.ending_score = 10000
+
+
+class Player:
+    def __init__(self, s=500, ws=10000, name='p1', user_input=False):
+        self.score = 0
+        self.turns = 0
+        self.num_of_dice = 6
+        self.playerName = name
+        self.score_to_stop_at_turn = s
+        self.game_end_score = ws
+        self.user_input = user_input
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        # assuming score to stop at is the strategy
+        # could also go by number of dice
+        score = 0
+        num_dice = self.num_of_dice
+        if(self.score >= self.game_end_score):
+            raise StopIteration
+        while(num_dice>0):
+            dice_rolled = roll_n_die(6)
+            print(f'{self.playerName} rolled {dice_rolled}')
+            pts, remaining = score_die(dice_rolled)
+            if self.user_input and pts>0:
+                print('do you want to end your turn(y/n)?')
+                resp = str(input())
+                if 'y' in resp or 'Y' in resp:
+                    self.score += score
+                    return score
+                print('how many die to remove?')
+                n = int(input())
+                for i in range(n):
+                    dice_rolled.remove(int(input()))
+                pts, remaining = score_die(dice_rolled)
+            else:
+                if pts>0:
+                    score += pts
+                    num_dice -= remaining
+                else:
+                    print(f'{self.playerName}\'s turn ended with {score} points')
+                    return 0
+        self.score += score
+        return score
+        
+
+if __name__ == "__main__":
+    p1 = Player()
+    print(next(p1))
+    
